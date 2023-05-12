495839235de8c4352fa638e4986ed831110a861a
Reece Yang <reeceyang@live.com>
2023-05-11 21:10:30 -0400

    reformat the file

--- bot.py
+++ bot.py
@@ -14,19 +14,23 @@ channels = set()
 client = pymongo.MongoClient(os.getenv('MONGODB_CONN_STR'), serverSelectionTimeoutMS=5000)
 db = client.Cluster0
 
+
 def read_config():
     """returns the json object stored in config.js"""
     with open('config.json') as f:
         return json.loads(f.read())
+
 
 if read_config()['production']:
     collection = db.confessions
 else:
     collection = db.testing
 
+
 def insert_confession(post, number):
     confession = {**post, 'number': number}
-    print('inserted',number,'with id',collection.insert_one(confession).inserted_id)
+    print('inserted', number, 'with id', collection.insert_one(confession).inserted_id)
+
 
 def insert_confessions(posts):
     for post in posts:
@@ -36,6 +40,7 @@ def insert_confessions(posts):
         except ValueError:
             continue
         insert_confession(post, text)
+
 
 try:
     print(client.server_info())
@@ -47,10 +52,12 @@ def read_storage():
     with open('storage.json') as f:
         return json.loads(f.read())
 
+
 def dump_storage():
     with open('storage.json', 'w') as f:
         f.write(json.dumps({'last': last_number, 'channels': list(channels)}))
-          
+
+
 if read_config()['production']:
     TOKEN = os.getenv('DISCORD_TOKEN')
 else:
@@ -68,6 +75,8 @@ except FileNotFoundError:
     dump_storage()
 
 client = discord.Client()
+
+
 # def get_post_text(text):
 #    return text
 #    FIXME: why is facebook-scraper like this
@@ -96,6 +105,7 @@ def format_confession(post):
     text += '\n<' + link + '>'
     return split_confession(text)
 
+
 def split_confession(text):
     if len(text) <= 2000:
         return [text]
@@ -107,19 +117,23 @@ def split_confession(text):
                 split_index -= 1
         return [text[:split_index + 1]] + split_confession(text[split_index + 1:])
 
+
 def bold_number(text):
     first_space = text.index(" ")
     return "**" + text[0:first_space] + "**" + text[first_space:]
 
-def get_number(text): 
+
+def get_number(text):
     # HACK: wtf mit confessions admin
     if "#64205I" in text:
         return 64205
     return int(text[1:text.index(" ")])
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
 
 async def update_confessions():
     """gets new confessions, posts them to all connected channels, and inserts them into database"""
@@ -136,9 +150,10 @@ async def update_confessions():
     for channel_id in channels:
         channel = client.get_channel(channel_id)
         await post_confessions(posts, channel)
-        if missed_confessions > 0: 
+        if missed_confessions > 0:
             await channel.send(missed_string)
     return True
+
 
 def get_new_posts():
     """attempts to get any new confessions"""
@@ -153,6 +168,7 @@ def get_new_posts():
         pages *= 2
     return posts, lowest_number, max_number
 
+
 async def post_confessions(posts, channel):
     """post all confessions in `posts` to `channel`"""
     for post in posts:
@@ -162,20 +178,21 @@ async def post_confessions(posts, channel):
                 await channel.send(response)
         except Exception as e:
             print(str(e))
- 
+
+
 def get_confessions(num_pages, stop_number=None):
     """return a list of posts, the lowest number retrieved, and the highest confession number retrieved"""
     posts = []
     print("getting confessions")
-    min_number = float('inf') 
-    max_number = stop_number 
+    min_number = float('inf')
+    max_number = stop_number
     is_error = False
     try:
         for post in get_posts('beaverconfessions', cookies="cookies-facebook-com.txt", pages=num_pages):
             text = post['post_text']
             # ignore pinned post
-            if not text or text[0] != "#": 
-                continue 
+            if not text or text[0] != "#":
+                continue
             try:
                 number = get_number(text)
             except ValueError:
@@ -185,16 +202,18 @@ def get_confessions(num_pages, stop_number=None):
             if stop_number is not None and number <= stop_number:
                 break
             print("got confession", number)
-            posts.insert(0, post) 
+            posts.insert(0, post)
         assert max_number != 0
         assert min_number < float('inf')
     except Exception:
         is_error = True
     return posts, min_number, max_number, is_error
 
+
 async def show_recent_confessions(channel):
     posts, _, _ = get_confessions(2)
     await post_confessions(posts, channel)
+
 
 @client.event
 async def on_message(message):
@@ -210,18 +229,20 @@ async def on_message(message):
                 await message.channel.send('new confessions will be posted in #' + message.channel.name)
             if 'remove' in message.content:
                 channels.discard(message.channel.id)
-                await message.channel.send('new confessions will not be posted in #' + message.channel.name) 
+                await message.channel.send('new confessions will not be posted in #' + message.channel.name)
             dump_storage()
-            
+
+
 @tasks.loop(minutes=60)
 async def my_background_task():
     if not await update_confessions():
         print('no new confessions')
 
+
 @my_background_task.before_loop
 async def my_background_task_before_loop():
     await client.wait_until_ready()
 
+
 my_background_task.start()
 client.run(TOKEN)
-
